7f7030990f03f53fb132bf6c31bc16ccc022c6b4
TieuGiaHuy <huytg.ce191594@gmail.com>
2025-07-04 14:13:56 +0700

    update User

--- Users.sql
+++ Users.sql
@@ -1,12 +1,19 @@
-﻿CREATE TABLE Users (
-    username VARCHAR(50) PRIMARY KEY,
-    full_name NVARCHAR(100),
-    email VARCHAR(100),
-    password VARCHAR(100),
-    role VARCHAR(20) -- 'admin' hoặc 'customer'
+﻿use vlxd
+go
+
+CREATE TABLE [User] (
+    id INT IDENTITY(1,1) PRIMARY KEY,        
+    username VARCHAR(50) NOT NULL UNIQUE,    -- Tên đăng nhập không được trùng
+    fullName NVARCHAR(100) NOT NULL,          
+    email VARCHAR(100) NOT NULL,              
+    password VARCHAR(100) NOT NULL,           
+    role VARCHAR(20) DEFAULT 'Customer'      -- Vai trò: 'Admin' hoặc 'Customer'
 );
 
 INSERT INTO Users (username, full_name, email, password, role)
 VALUES 
 ('admin01', N'Admin', 'admin@example.com', 'admin123', 'admin'), -- 'tk admin'
 ('huy123', N'Huy Tiêu', 'huy@example.com', '123456', 'customer'); -- 'tk customer'
+
+
+
