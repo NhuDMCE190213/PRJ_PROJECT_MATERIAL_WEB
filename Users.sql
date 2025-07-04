e90a8e7018f44f7f64b720647a8a57f3537c6b62
TieuGiaHuy <huytg.ce191594@gmail.com>
2025-07-04 09:38:08 +0700

    thêm bảng user

---
+++ Users.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE Users (
+    username VARCHAR(50) PRIMARY KEY,
+    full_name NVARCHAR(100),
+    email VARCHAR(100),
+    password VARCHAR(100),
+    role VARCHAR(20) -- 'admin' hoặc 'customer'
+);
+
+INSERT INTO Users (username, full_name, email, password, role)
+VALUES 
+('admin01', N'Admin', 'admin@example.com', 'admin123', 'admin'), -- 'tk admin'
+('huy123', N'Huy Tiêu', 'huy@example.com', '123456', 'customer'); -- 'tk customer'
